9bc61beeae1abee772fbd56acd019bb24685eb97
Dorina <dorina.platon2014@gmail.com>
2024-08-01 16:42:39 +0300

    Tema

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        LogicalOP op = new LogicalOP();
+        Calculator calculator = new Calculator();
+
+        calculator.sumOfMyNumbers();
+        calculator.divOfMyNumbers();
+        calculator.resultsOfOperations();
+        System.out.println("Addition: " + calculator.addition(3, 9));
+        System.out.println("Substraction: " + calculator.substraction(345, 161));
+        System.out.println("Multiplication: " + calculator.multiplication(12, 54));
+        System.out.println("Division: " + calculator.division(234, 56));
+        System.out.println("Average: " + calculator.average(123, 342, 12));
+        System.out.println("Remainder: " + calculator.remainder(154, 34));
+        System.out.println("Temperature in Celsius: " + calculator.temp(564));
+        System.out.println("Distance in meters" + calculator.distance(1343));
+        calculator.speed(9832, 0, 24, 33);
+        calculator.circleAreaAndPerim();
+        calculator.image();
+
+        System.out.println("Bigger number is: " + op.checkHigherNumber(3, 7));
+        System.out.println(op.textCompare("evozon"));
+        System.out.println(op.textAndNumCompare("Evozon", 3));
+        System.out.println(op.snowAmount(9));
+        System.out.println(op.numberCompare(3.999999F));
+        System.out.println(op.numbersCase(43));
+        System.out.println(op.isNumberEven(0));
+        System.out.println("Is this person eligible to vote: " + op.isEligibleToVote(18));
+        System.out.println("The largest of the numbers is: " + op.largestNumberOfThree(5, 12, 32));
+
+        op.countToMinusHundred(10);
+        op.countBetweenNr(13, 21);
+        op.countAscending(35, 45);
+        op.evenNumbersFrom1To100();
+        op.oddNumbersFrom1To100();
+
+
+    }
+
+}
+
